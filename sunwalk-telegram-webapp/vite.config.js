ae89f7795bd874c7fd8f97b0a38ec51f493052f3
Naid888 <dmitrii.naidenov8@gmail.com>
2025-06-26 07:43:29 +0800

    Update vite.config.js

--- sunwalk-telegram-webapp/vite.config.js
+++ sunwalk-telegram-webapp/vite.config.js
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/'
+  base: '/',
+  build: {
+    outDir: 'dist'
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 8080
+  }
 })
