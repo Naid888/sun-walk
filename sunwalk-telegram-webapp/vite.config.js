d00f3329496c7eddf85bc36e58fb821d7a67f667
Naid888 <dmitrii.naidenov8@gmail.com>
2025-06-26 09:09:37 +0800

    Update vite.config.js

--- sunwalk-telegram-webapp/vite.config.js
+++ sunwalk-telegram-webapp/vite.config.js
@@ -2,16 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/',
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production'
-    ? 'https://sun-walk-production-b3af.up.railway.app/'
-    : '/',
   server: {
     host: true,
   },
   preview: {
     host: true,
     port: 8080,
-    allowedHosts: ['sun-walk-production-b3af.up.railway.app'],
+    allowedHosts: ['sun-walk-production-b3af.up.railway.app'], // подставь свой Railway-домен
   },
 })
