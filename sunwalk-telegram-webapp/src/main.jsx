eb88696238a56ba2dfa042c48e309f7d63a5e5bb
Naid888 <dmitrii.naidenov8@gmail.com>
2025-06-29 04:35:47 +0800

    Update main.jsx

--- sunwalk-telegram-webapp/src/main.jsx
+++ sunwalk-telegram-webapp/src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css' // если есть стили
+import App from './App'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// Telegram WebApp API
+window.Telegram.WebApp.ready()
+
+const root = document.getElementById('root')
+
+ReactDOM.createRoot(root).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
