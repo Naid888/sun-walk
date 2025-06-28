fa801a900560b5c125e668d6f905f562a298a13b
Naid888 <dmitrii.naidenov8@gmail.com>
2025-06-29 04:54:21 +0800

    Update index.tsx

--- sunwalk-telegram-webapp/src/index.tsx
+++ sunwalk-telegram-webapp/src/index.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter><App /></BrowserRouter>
-);
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Sun Walk</title>
+    <script src="https://telegram.org/js/telegram-web-app.js"></script>
+  </head>
+  <body>
+    <div id="root"></div>
+    <script type="module" src="/src/main.jsx"></script>
+  </body>
+</html>
